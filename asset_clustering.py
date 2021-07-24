2ebf00c5640d1358709e587a2e9cb31fb5a0fe5a
KayleeZhu <kayleechu@hotmail.com>
2021-07-24 12:24:11 -0600

    added kmeans cluster

--- asset_clustering.py
+++ asset_clustering.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 from sklearn.cluster import OPTICS
-from sklearn.neighbors import NearestNeighbors
+from sklearn.cluster import KMeans
 import generate_pca_features as fea
 
 
@@ -11,15 +11,28 @@ class Cluster:
         self.training_date = training_date
         self.pca_features = pca_features
 
-    def knn(self, n_neighbors):
+    def kmeans(self, n_clusters):
         # TODO：need to choose n, see which n works better
-        neigh = NearestNeighbors(n_neighbors)
-        neigh.fit(self.pca_features)
         # TODO: reformat the code to make it has the same output as optics
+
+        # Get the training date PCA features data
+        pca_for_the_date = self.pca_features.loc[self.pca_features['date'] == self.training_date].copy()
+        cluster = KMeans(n_clusters).fit(pca_for_the_date)
+
+        # Join the cluster label to the PCA data for the training date
+        pca_for_the_date = pca_for_the_date.join(
+            pd.DataFrame(data=cluster.labels_, index=pca_for_the_date.index, columns=['cluster_label']))
+
+        # Drop the PCA values (col) and  drop the assets with cluster label = -1 (outliers)
+        cluster_label_all_assets = pca_for_the_date[['GVKEY', 'date', 'cluster_label']]
+        cluster_label_all_assets = cluster_label_all_assets[cluster_label_all_assets.cluster_label != -1]
+        # print(cluster.labels_)
+
+        return cluster_label_all_assets
 
     def optics(self):
         # Get the training date PCA features data
-        pca_for_the_date = self.pca_features.loc[self.pca_features['date'] == self.training_date]
+        pca_for_the_date = self.pca_features.loc[self.pca_features['date'] == self.training_date].copy()
         cluster = OPTICS().fit(pca_for_the_date[[col for col in pca_for_the_date.columns if 'pca' in col]])
 
         # Join the cluster label to the PCA data for the training date
@@ -33,7 +46,6 @@ class Cluster:
 
         # TODO: how to visualize the cluster?
         return cluster_label_all_assets
-
 
 
 if __name__ == '__main__':
